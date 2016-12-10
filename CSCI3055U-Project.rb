e1ae740a07b72cabc6fa04ab7701f8a3b507fea9
khirthana <khirthana.subramanian@uoit.net>
2016-12-09 19:01:40 -0500

    display schedule for all courses

--- CSCI3055U-Project.rb
+++ CSCI3055U-Project.rb
@@ -15,53 +15,95 @@ Shoes.app :title => "Schedule Builder App", :width => 640, :height => 430 do
       button "Get schedule",:width => 100  do
 
   #para @edit.text
-        alert "button clicked"
-        @box = edit_box :width => 0.97, :height => 80, :text =>'',:margin_left => '2%'
+        if (@edit.text).eql? ""
+          alert "Enter course code!"
+        elsif (@edit.text).eql? "all"
+          @box = edit_box :width => 0.97, :height => 80, :text =>'',:margin_left => '2%'
 
-        @file = File.new("schedule.txt", "r")
-        @elements = Array.new
-        @schedules = Array.new
-        counter = 0
+          @file = File.new("schedule.txt", "r")
+          @elements = Array.new
+          @schedules = Array.new
+          counter = 0
 
-        @output="Schedule for "+ @edit.text+": \n"
-        #@output="Schedule for CSCI 3055U: \n"
-        @day=""
+          @output="Schedule for "+ @edit.text+": \n"
+          @day=""
+        #search for course code input by user and extract schedule from the schedule.txt file
+          while (@line = @file.gets)
+              @elements.insert(counter,@line)
+              @e=@line.split("|")
 
-      #search for course code input by user and extract schedule from the schedule.txt file
-        while (@line = @file.gets)
-          #if @line.include? @edit.text
-          if @line.include? "CSCI 3055U"
-            @elements.insert(counter,@line)
-            @e=@line.split("|")
+        #format and display the day of class for user
+              puts case @e[4]
+              when "M"
+                @day="Thursday"
+              when "T"
+                @day="Tuesday"
+              when "W"
+                @day="Wednesday"
+              when "R"
+                @day="Thursday"
+              when "F"
+                @day="Friday"
+              else
+                @day="Saturday"
+              end
 
-      #format and display the day of class for user
-            puts case @e[4]
-            when "M"
-              @day="Thursday"
-            when "T"
-              @day="Tuesday"
-            when "W"
-              @day="Wednesday"
-            when "R"
-              @day="Thursday"
-            when "F"
-              @day="Friday"
-            else
-              @day="Saturday"
-            end
+              @schedule=counter.to_s+" : "+@day+" "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+" "+@e[2]+" "+@e[1]+" "+@e[8]
 
-            @schedule=counter.to_s+" : "+@day+" "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+" "+@e[2]+" "+@e[1]+" "+@e[8]
+              @schedules.insert(counter,@schedule)
 
-            @schedules.insert(counter,@schedule)
-
-            @output=@output+@schedule
-            counter = counter + 1
+              @output=@output+@schedule
+              counter = counter + 1
           end
+            #prints schedule to user
+            @box.text= @output
+        else
+          @box = edit_box :width => 0.97, :height => 80, :text =>'',:margin_left => '2%'
+
+          @file = File.new("schedule.txt", "r")
+          @elements = Array.new
+          @schedules = Array.new
+          counter = 0
+
+          @output="Schedule for "+ @edit.text+": \n"
+          #@output="Schedule for CSCI 3055U: \n"
+          @day=""
+
+        #search for course code input by user and extract schedule from the schedule.txt file
+          while (@line = @file.gets)
+            #if @line.include? @edit.text
+            if @line.include? "CSCI 3055U"
+              @elements.insert(counter,@line)
+              @e=@line.split("|")
+
+        #format and display the day of class for user
+              puts case @e[4]
+              when "M"
+                @day="Thursday"
+              when "T"
+                @day="Tuesday"
+              when "W"
+                @day="Wednesday"
+              when "R"
+                @day="Thursday"
+              when "F"
+                @day="Friday"
+              else
+                @day="Saturday"
+              end
+
+              @schedule=counter.to_s+" : "+@day+" "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+" "+@e[2]+" "+@e[1]+" "+@e[8]
+
+              @schedules.insert(counter,@schedule)
+
+              @output=@output+@schedule
+              counter = counter + 1
+            end
+          end
+
+        #prints schedule to user
+          @box.text= @output
         end
-
-      #prints schedule to user
-        @box.text= @output
-
       end
     end
   end
