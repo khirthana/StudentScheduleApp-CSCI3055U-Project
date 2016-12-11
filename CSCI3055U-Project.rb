9ecfe46486cb0a5463e47f6acf03609525895bc1
khirthana <khirthana.subramanian@uoit.net>
2016-12-11 00:09:32 -0500

    save multiple schedules to csv

--- CSCI3055U-Project.rb
+++ CSCI3055U-Project.rb
@@ -6,16 +6,14 @@
 
 require 'csv'
 
-
+$counter=0
 
 #method to extract schedule for user input course code using schedule.txt file
 def get_schedule(input)
-
+	 
 	  @file = File.new("schedule.txt", "r")
 	  @elements = Array.new
 	  @schedules = Array.new
-	  
-	  @counter = 0
 
 	  @output_lines="Schedule for "+ input+": \n"
 	
@@ -25,7 +23,7 @@ def get_schedule(input)
 	  while (@line = @file.gets)
 	  
 		if @line.include? input
-		  @elements.insert(@counter,@line)
+		  @elements.insert($counter,@line)
 		  @e=@line.split("|")
 
 	#format and display the day of class for user
@@ -44,12 +42,12 @@ def get_schedule(input)
 			@day="Saturday"
 		  end
 
-		  @schedule=@counter.to_s+" :   "+@day+"   "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+"   "+@e[2]+" "+@e[1]+"   "+@e[8]
+		  @schedule=$counter.to_s+" :   "+@day+"   "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+"   "+@e[2]+" "+@e[1]+"   "+@e[8]
 
-		  @schedules.insert(@counter,@schedule)
+		  @schedules.insert($counter,@schedule)
 
 		  @output_lines=@output_lines+@schedule
-		  @counter = @counter + 1
+		  $counter = $counter + 1
 		end
 	  end
 
@@ -67,56 +65,60 @@ end
 
 def save_schedule(input)
 
- CSV.open("MySchedule.csv", "w") do |csv|
-		  csv << ["Subject", "Start Date", "Start Time", "End Date","End Time","All Day Event","Description","Location","Private"]
-		  
-		  @a=@schedules[input.to_i].split("   ")
-		  
-		  
-		  @date=""
-		  puts case @a[1]
-		  when "Monday"
-			@date="09/12/2016"
-		  when "Tuesday"
-			@date="09/13/2016"
-		  when "Wednesday"
-			@date="09/14/2016"
-		  when "Thursday"
-			@date="09/08/2016"
-		  when "Friday"
-			@date="09/09/2016"
-		  else
-			@date="09/10/2016"
-		  end
+ @input_array=input.split(",")
+ 
+		
+	
+	 CSV.open("MySchedule.csv", "w") do |csv|
+			  csv << ["Subject", "Start Date", "Start Time", "End Date","End Time","All Day Event","Description","Location","Private"]
+			  for @index in 0 ... (@input_array.size)
+				  @a=(@schedules[(@input_array[@index]).to_i]).split("   ")
+				  
+				  
+				  @date=""
+				  puts case @a[1]
+				  when "Monday"
+					@date="09/12/2016"
+				  when "Tuesday"
+					@date="09/13/2016"
+				  when "Wednesday"
+					@date="09/14/2016"
+				  when "Thursday"
+					@date="09/08/2016"
+				  when "Friday"
+					@date="09/09/2016"
+				  else
+					@date="09/10/2016"
+				  end
 
-		  @time=@a[2].split("-")
-		  @start=@time[0].split(":")
-		  @end=@time[1].split(":")
-	  
-	       if ((@end[1]).size)<2
-			@end[1]=@end[1]+"0"
-		   end
-		   
-		   if ((@start[0]).to_i)<12 
-			@start_time=@time[0]+" AM"
-		   elsif (@start[0]).eql? "12"
-			@start_time=@time[0]+" PM"
-		   else
-		    @start_time=(((@start[0]).to_i)-12).to_s+":"+@start[1]+" PM"
-		   end
-		   
-		   if ((@end[0]).to_i)<12 
-			@end_time=@time[1]+" AM"
-		   elsif (@end[0]).eql? "12"
-			@end_time=@time[1]+" PM"
-		   else
-		    @end_time=(((@end[0]).to_i)-12).to_s+":"+@end[1]+" PM"
-		   end
-		  
-		  csv << [@a[3], @date, @start_time, @date,@end_time,"False","",@a[4],"True"]
-		  
+				  @time=@a[2].split("-")
+				  @start=@time[0].split(":")
+				  @end=@time[1].split(":")
+			  
+				   if ((@end[1]).size)<2
+					@end[1]=@end[1]+"0"
+				   end
+				   
+				   if ((@start[0]).to_i)<12 
+					@start_time=@time[0]+" AM"
+				   elsif (@start[0]).eql? "12"
+					@start_time=@time[0]+" PM"
+				   else
+					@start_time=(((@start[0]).to_i)-12).to_s+":"+@start[1]+" PM"
+				   end
+				   
+				   if ((@end[0]).to_i)<12 
+					@end_time=@time[1]+" AM"
+				   elsif (@end[0]).eql? "12"
+					@end_time=@time[1]+" PM"
+				   else
+					@end_time=(((@end[0]).to_i)-12).to_s+":"+@end[1]+" PM"
+				   end
+				  
+				  csv << [@a[3], @date, @start_time, @date,@end_time,"False","",@a[4],"True"]
+				  
+				end
 		end
-	  
 end
 
 #GUI using Shoes
