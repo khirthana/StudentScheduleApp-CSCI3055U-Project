75db8235816cb09be3b3ec83adb929f7c7c6d44c
khirthana <khirthana.subramanian@uoit.net>
2016-12-10 21:20:23 -0500

    added method to get schedule

--- CSCI3055U-Project.rb
+++ CSCI3055U-Project.rb
@@ -2,24 +2,90 @@
 #Khirthana Subramanian
 #100453865
 
-Shoes.app :title => "Schedule Builder App", :width => 800, :height => 550 do
+
+#method to extract schedule for user input course code using schedule.txt file
+def get_schedule(input)
+
+	  @file = File.new("schedule.txt", "r")
+	  @elements = Array.new
+	  @schedules = Array.new
+	  counter = 0
+
+	  @output_lines="Schedule for "+ input+": \n"
+	
+	  @day=""
+
+	#search for course code input by user and extract schedule from the schedule.txt file
+	  while (@line = @file.gets)
+		if @line.include? input
+		#if @line.include? "CSCI 3055U"
+		  @elements.insert(counter,@line)
+		  @e=@line.split("|")
+
+	#format and display the day of class for user
+		  puts case @e[4]
+		  when "M"
+			@day="Thursday"
+		  when "T"
+			@day="Tuesday"
+		  when "W"
+			@day="Wednesday"
+		  when "R"
+			@day="Thursday"
+		  when "F"
+			@day="Friday"
+		  else
+			@day="Saturday"
+		  end
+
+		  @schedule=counter.to_s+" : "+@day+" "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+" "+@e[2]+" "+@e[1]+" "+@e[8]
+
+		  @schedules.insert(counter,@schedule)
+
+		  @output_lines=@output_lines+@schedule
+		  counter = counter + 1
+		end
+	  end
+
+	  @file.close
+	  
+	  if @schedules.empty?
+		@output_lines=""
+	 
+	  end
+	  
+	  return @output_lines
+	  
+end
+
+
+#GUI using Shoes
+Shoes.app :title => "Student Schedule Builder App", :width => 800, :height => 550 do
 
   background whitesmoke
   border(darkblue,
          strokewidth: 6)
+		 
+		 
   stack(margin_left: 12,margin_top:10) do
-    @AppName = para "Schedule Builder App"
+    @AppName = para "Student Schedule Builder"
     @AppName.style(stroke: darkblue,:align=>"center"  ,font: "Verdana Italic 24px" )
   end
+  
+  
   stack(margin_left: '45%',margin_bottom:10) do
     @img = image 'uoit-logo.jpg'
     @img.style(height:50,width:80,:align=>"center")
   end
+  
+  
   stack(margin_left: 12) do
     button "Info",:margin_left => '45%' do
-      alert "Schedule Builder App\n\n   Enter course code and click -Get schedule- button to display schedule. \n\n   To view course schedule for course, enter course code(CSCI 3055U) \nOR\n   enter all for all course schedules \nOR\n   enter program code (CSCI or SOFE) for all program course schedules\n\nDeveloped by: Khirthana Subramanian 100453865"
+      alert "Student Schedule Builder App\n\n   Enter course code and click -Get schedule- button to display schedule. \n\n   To view course schedule for course, enter course code(CSCI 3055U) \nOR\n   enter all for all course schedules \nOR\n   enter program code (CSCI or SOFE) for all program course schedules\n\nDeveloped by: Khirthana Subramanian 100453865"
     end
   end
+  
+  
     stack(margin: 12) do
     flow do
       para "Enter course code (ie: CSCI 3055U): ",stroke: midnightblue
@@ -29,106 +95,37 @@ Shoes.app :title => "Schedule Builder App", :width => 800, :height => 550 do
 
       button "Get schedule",:width => 100  do
 
-        if (@edit.text).empty? or (@edit.text).size<4 or (@edit.text).size>9
+        if (@edit.text).empty? or (@edit.text).size>10
           alert "Enter course code!"
-
-        elsif (@edit.text).eql? "all"
-          @box = edit_box :width => 0.97, :height => 80, :text =>'',:margin_left => '2%'
-
-          @file = File.new("schedule.txt", "r")
-          @elements = Array.new
-          @schedules = Array.new
-          counter = 0
-
-          @output="Schedule for "+ @edit.text+": \n"
-          @day=""
-        #search for course code input by user and extract schedule from the schedule.txt file
-          while (@line = @file.gets)
-              @elements.insert(counter,@line)
-              @e=@line.split("|")
-
-        #format and display the day of class for user
-              puts case @e[4]
-              when "M"
-                @day="Thursday"
-              when "T"
-                @day="Tuesday"
-              when "W"
-                @day="Wednesday"
-              when "R"
-                @day="Thursday"
-              when "F"
-                @day="Friday"
-              else
-                @day="Saturday"
-              end
-
-              @schedule=counter.to_s+" : "+@day+" "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+" "+@e[2]+" "+@e[1]+" "+@e[8]
-
-              @schedules.insert(counter,@schedule)
-
-              @output=@output+@schedule
-              counter = counter + 1
-          end
-
-          @file.close
-
+		  
+		elsif (@edit.text).size<4 and (@edit.text).eql? "all"
+        
+		  @output=get_schedule("")
+		  
           #prints schedule to user
+		  @box = edit_box :width => 0.97, :height => 80, :text =>'',:margin_left => '2%'
           @box.text= @output
-
+ 
+		elsif (@edit.text).size<4 
+			alert "Enter course code!"
+			
         else
-          @box = edit_box :width => 0.97, :height => 80, :text =>'',:margin_left => '2%'
+          @output=get_schedule(@edit.text)
 
-          @file = File.new("schedule.txt", "r")
-          @elements = Array.new
-          @schedules = Array.new
-          counter = 0
-
-          @output="Schedule for "+ @edit.text+": \n"
-          #@output="Schedule for CSCI 3055U: \n"
-          @day=""
-
-        #search for course code input by user and extract schedule from the schedule.txt file
-          while (@line = @file.gets)
-            #if @line.include? @edit.text
-            if @line.include? "CSCI 3055U"
-              @elements.insert(counter,@line)
-              @e=@line.split("|")
-
-        #format and display the day of class for user
-              puts case @e[4]
-              when "M"
-                @day="Thursday"
-              when "T"
-                @day="Tuesday"
-              when "W"
-                @day="Wednesday"
-              when "R"
-                @day="Thursday"
-              when "F"
-                @day="Friday"
-              else
-                @day="Saturday"
-              end
-
-              @schedule=counter.to_s+" : "+@day+" "+@e[3]+":"+@e[5]+"-"+@e[6]+":"+@e[7]+" "+@e[2]+" "+@e[1]+" "+@e[8]
-
-              @schedules.insert(counter,@schedule)
-
-              @output=@output+@schedule
-              counter = counter + 1
-            end
-          end
-
-          @file.close
-
-        #prints schedule to user
-          @box.text= @output
-
+		  if @output.empty?
+			alert "Enter valid course code!"
+		  else
+			#prints schedule to user
+			@box = edit_box :width => 0.97, :height => 80, :text =>'',:margin_left => '2%'
+			@box.text= @output
+		 end
+		 
         end
       end
     end
   end
+  
+  
   stack(margin: 12) do
     flow do
       para "Enter schedule line number:",stroke: midnightblue
@@ -136,6 +133,7 @@ Shoes.app :title => "Schedule Builder App", :width => 800, :height => 550 do
       @edit2.style(width => 100)
       button "Save schedule to CSV file",:width => 200 do
       end
+	  
     end
   end
 end
